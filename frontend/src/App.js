d539a14c6075eacd28a7f08966789854e4e6aa7f
Daviti Vashakmadze <vashaymadzedavit@gmail.com>
2024-10-19 23:12:17 +0400

    add route pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Update this line
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'; // Update this line
 import Login from './components/login/Login';
 import Register from './components/register/Register';
 
@@ -6,9 +6,19 @@ function App() {
     return (
         <Router>
             <div className="App">
+            <nav>
+                    <ul>
+                        <li>
+                            <Link to="/login">Login</Link>
+                        </li>
+                        <li>
+                            <Link to="/register">Register</Link>
+                        </li>
+                    </ul>
+                </nav>
                 <Routes> {/* Change Switch to Routes */}
-                    <Route path="/login" element={<Login />} /> {/* Update Route component */}
-                    <Route path="/register" element={<Register />} /> {/* Update Route component */}
+                    <Route path="/login" element={<Login/>} /> {/* Update Route component */}
+                    <Route path="/register" element={<Register/>} /> {/* Update Route component */}
                 </Routes>
             </div>
         </Router>
