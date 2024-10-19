2cde55949be4f207c509a5fdac4b0d622b3cfd16
Daviti Vashakmadze <vashaymadzedavit@gmail.com>
2024-10-19 21:41:04 +0400

    fix: fix mondgodb server conection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Update this line
+import Login from './components/login/Login';
+import Register from './components/register/Register';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Routes> {/* Change Switch to Routes */}
+                    <Route path="/login" element={<Login />} /> {/* Update Route component */}
+                    <Route path="/register" element={<Register />} /> {/* Update Route component */}
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
